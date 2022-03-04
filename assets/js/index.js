7c51ffef0ec30a66cec050ecf00e9d81b572a862
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-05 00:43:39 +0300

    finding js script. test 1

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var myDate = new Date(),
+    month = myDate.getMonth(),
+    date = myDate.getDate(),
+    day = myDate.getDay();
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+console.log(days[day]);
