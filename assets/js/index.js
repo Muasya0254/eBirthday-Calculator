29d0faf033751946d275b3f58d0c02e716e2454b
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-05 21:22:48 +0300

    alertUser working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,32 @@
-var myDate = new Date(),
-    month = myDate.getMonth(),
-    date = myDate.getDate(),
-    day = myDate.getDay();
+
 
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var myDate = new Date(),
+day = myDate.getDay();
 console.log(days[day]);
+
+document.getElementById("alertUser").innerHTML = "You we born on a " + (days[day]);
+
+var dob = document.getElementById("date").value;
+
+var akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+var akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+
+let akanFem;
+
+
+function processData() {
+    document.getElementById("submitInput").value;
+
+}
+
+function idGender() {
+    document.getElementById("gender").value;
+
+}
+
+function resetform() {
+    document.getElementById("form").reset();
+
+}
