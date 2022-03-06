c0cf315c41cc3e5ebbd1ea0a4ea4996367a2c2d5
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-06 20:00:43 +0300

    deleted files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+function resetform() {
+    document.getElementById("form").innerHTML = "";
+}
+
+function createDate (year, month, _date) {
+    var d = new Date(year, month, _date);
+    if (d.getFullYear() != year 
+      || d.getMonth() != month
+      || d.getDate() != _date) {
+      throw "invalid date";
+    }
+    return d;
+  }
 
 function birthDayAlert() {
 var myform = document.forms.akanNameForm;
@@ -7,17 +20,31 @@ var akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
 var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] //day of week by name
 var birthday = document.getElementById = ("userBirthday").value; //get date from form
 var dateOfBirth = new Date (birthday); // use date from form
-var dayOfweek = dateOfBirth.getDay(); //day of week in number
-var usergender = document.getElementsByClassName = ("gender"); //call gender
+var dayNumber = dateOfBirth.getDay(); //day of week in number
+var userGender = document.getElementsByClassName = ("gender"); //call gender
 
-
-
-
-
+/// if conditions
+if (birthday === "") //strict equality operator
+{
+    document.getElementById('AkanName').innerHTML;
 }
+else {
+        for (var i=0;i<userGender.length;i++)
+        { //loop
+        if(userGender[i].checked)
+        {
+                if(userGender[i].value === "userFemale")
+                    {
+                    document.getElementById('alertUser').innerHTML = "You were born on a " + daysOfTheWeek[dayNumber] + "your Akan Name is " + akanFemaleNames[dayNumber];
+                    }
+                 else{
+                        document.getElementById('alertUser').innerHTML = "You were born on a " + daysOfTheWeek[dayNumber] + "your Akan Name is " + akanMaleNames[dayNumber] ;
+                     }
+                    }
+                else if(userGender[0].checked==false && userGender[1].checked==false ) {
+                        alert("You must select male or female");
+                        return false;
+        }
+    }
 
-////////
-
-    function resetform() {
-    document.getElementById("form").reset();
-}
+//////// reset button
